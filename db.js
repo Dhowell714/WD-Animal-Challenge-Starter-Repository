e565e0d4ad7cb98f59c53bdbfd5a7f1c5f403154
Devin Howell <lunarwolfalpha714@gmail.com>
2021-11-02 20:59:23 -0400

    1

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const {Sequelize} = require('sequelize');
 
-const db = new Sequelize("postgres://postgres:YOURPASSWORDHERE@localhost:5432/animal-server");
+const db = new Sequelize("postgres://postgres:c188645ec8f3419abf51129081469837@localhost:5432/animal-server");
 
 module.exports = db;
